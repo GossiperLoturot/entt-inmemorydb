ae47f8f0777aad12dff26a60b4941b8f0d99df31
Takumi Sugimoto <ts20030211@gmail.com>
2026-05-09 23:23:33 +0900

    use flatbuffers and asio instead of gRPC

--- include/greeter.h
+++ include/greeter.h
@@ -1,16 +1,40 @@
-#include <iostream>
-#include <grpcpp/grpcpp.h>
-#include "model.grpc.pb.h"
+#pragma once
 
-class GreeterServer final : public greet::Greeter::Service {
-public:
-    grpc::Status SayHello(
-        grpc::ServerContext* ctx,
-        const greet::HelloRequest* req,
-        greet::HelloReply* rep
-    ) override {
-        std::string prefix("Hello ");
-        rep->set_message(prefix + req->name());
-        return grpc::Status::OK;
+#include <iostream>
+#include <asio.hpp>
+#include <flatbuffers/flatbuffers.h>
+#include "model_generated.h"
+
+int run() {
+    try {
+        asio::io_context ctx{};
+        asio::ip::tcp::acceptor acceptor{ctx, asio::ip::tcp::endpoint{asio::ip::make_address("0.0.0.0"), 12345}};
+        std::cout << "server is waiting for connection." << std::endl;
+
+        asio::ip::tcp::socket socket{ctx};
+        acceptor.accept(socket);
+        std::cout << "client connected." << std::endl;
+
+        flatbuffers::FlatBufferBuilder builder{1024};
+        int id = 10;
+        auto vec3 = Model::Vec3{1.0f, 2.0f, 3.0f};
+        auto name = builder.CreateString("zero");
+        auto entity = Model::CreateEntity(builder, id, &vec3, name);
+        builder.Finish(entity);
+
+        uint8_t* buffer = builder.GetBufferPointer();
+        uint32_t size = builder.GetSize();
+        std::cout << "buffer size : " << size << std::endl;
+        
+        std::vector<asio::const_buffer> buffers{};
+        buffers.push_back(asio::buffer(&size, sizeof(size)));
+        buffers.push_back(asio::buffer(buffer, size));
+        asio::write(socket, buffers);
+
+    } catch (const std::exception& e) {
+        std::cerr << "server exception : " << e.what() << std::endl;
+        return -1;
     }
-};
+
+    return 0;
+}
--- src/main.cc
+++ src/main.cc
@@ -1,19 +1,10 @@
 #include <iostream>
-#include <grpcpp/grpcpp.h>
 #include "greeter.h"
 
 int main() {
     std::cout << "start task" << std::endl;
 
-    std::string server_addr = "0.0.0.0:50051";
-
-    GreeterServer service;
-    grpc::ServerBuilder builder;
-    builder.AddListeningPort(server_addr, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
-    std::unique_ptr<grpc::Server> handle(builder.BuildAndStart());
-    std::cout << "server listening on " + server_addr << std::endl;
-    handle->Wait();
+    run();
 
     std::cout << "complete task" << std::endl;
     return 0;
