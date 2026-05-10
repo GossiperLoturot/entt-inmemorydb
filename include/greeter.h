edab973a5f13201952ef490a62f1d80a79bc2760
Takumi Sugimoto <ts20030211@gmail.com>
2026-05-10 22:32:59 +0900

    make flatbuffer schemas

--- include/greeter.h
+++ include/greeter.h
@@ -16,11 +16,25 @@ int run() {
         std::cout << "client connected." << std::endl;
 
         flatbuffers::FlatBufferBuilder builder{1024};
-        int id = 10;
-        auto vec3 = Model::Vec3{1.0f, 2.0f, 3.0f};
-        auto name = builder.CreateString("zero");
-        auto entity = Model::CreateEntity(builder, id, &vec3, name);
-        builder.Finish(entity);
+        std::vector<flatbuffers::Offset<Model::CreateCommand>> create_cmds_vec{};
+        std::vector<flatbuffers::Offset<Model::UpdateCommand>> update_cmds_vec{};
+        std::vector<flatbuffers::Offset<Model::RemoveCommand>> remove_cmds_vec{};
+        for (int y = -32; y <= 32; ++y) {
+            for (int x = -32; x <= 32; ++x) {
+                int id = x + y * 32;
+                int type_id = 0;
+                auto position = Model::Vec3{x * 1.0f, 0.0f, y * 1.0f};
+                auto rotation = Model::Vec4{0.0f, 0.0f, 0.0f, 0.0f};
+                auto create_cmd = Model::CreateCreateCommand(builder, id, type_id, &position, &rotation);
+                create_cmds_vec.push_back(create_cmd);
+            }
+        }
+        int version = 0;
+        auto create_cmds = builder.CreateVector(create_cmds_vec);
+        auto update_cmds = builder.CreateVector(update_cmds_vec);
+        auto remove_cmds = builder.CreateVector(remove_cmds_vec);
+        auto cmds = Model::CreateCommands(builder, version, create_cmds, update_cmds, remove_cmds);
+        builder.Finish(cmds);
 
         uint8_t* buffer = builder.GetBufferPointer();
         uint32_t size = builder.GetSize();
